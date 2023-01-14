9f82a026cf086287c19b94edbb2c34e8911dbb45
Korvus <korvuskroax@gmail.com>
2023-01-14 18:39:13 +0100

    wip

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,10 +9,13 @@
 
     <title>QR code</title>
 </head>
+
 <body>
-    <h1>QR code</h1>
-    
-    <?php require("qrcode.php"); ?>
-    
+        <h1>QR code</h1>
+        <form action="qrcode.php" method="post">
+            <input type="text" name="content" required>
+            <input type="submit" name="submit" value="submit">
+        </form>
 </body>
+
 </html>
--- qrcode.php
+++ qrcode.php
@@ -1,30 +1,32 @@
 <?php
-require_once("phpqrcode/qrlib.php");
+    require_once("phpqrcode/qrlib.php");
 
-$path = "img/";
-$filename = time() . ".png";
+    $path = "img/";
+    $filename = time() . ".png";
+    $qrcode_image = $path . $filename;
 
-$qrcode = $path . $filename;
+    if (isset($_POST["submit"])) 
+    {
+        $qrcode_content = $_POST["content"];
 
-QRcode::png("Optometer Optika", $qrcode, "H");      // https://phpqrcode.sourceforge.net
+        QRcode::png($qrcode_content, $qrcode_image, "H", 10);      // https://phpqrcode.sourceforge.net
 
-echo "<img src='" . $qrcode . "'>";
+        echo "<img src='" . $qrcode_image . "'>";
 
-echo "<pre>";
-echo $qrcode;
-echo "</pre>";
-
-
-
-
-
-
-
+        echo "<pre>";
+        echo "content: " . $qrcode_content . "\n\n";
+        echo "image file: " . $qrcode_image;
+        echo "</pre>";
+    }
 
 
 
-// if not working (..Call to undefined function imagecreate() in..) 
-//  - open c:\xampp\php\php.ini
-//  - find ;extension=gd               and remove ;(semicolon)
-//  - find ;gd.jpeg_ignore_warning = 1 and remove ;(semicolon)
-//  - stop and start Apache
+
+
+
+
+    // if not working (..Call to undefined function imagecreate() in..)
+    // - open c:\xampp\php\php.ini
+    // - find ;extension=gd and remove ;(semicolon)
+    // - find ;gd.jpeg_ignore_warning = 1 and remove ;(semicolon)
+    // - stop and start Apache
