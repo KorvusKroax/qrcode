4abbfbd6fb8ded9d32d53257f0f703d8553f5bb9
Korvus <korvuskroax@gmail.com>
2023-01-14 18:11:20 +0100

    wip

--- qrcode.php
+++ qrcode.php
@@ -6,14 +6,25 @@ $filename = time() . ".png";
 
 $qrcode = $path . $filename;
 
-QRcode::png("Optometer Optika", $qrcode, "H", 4);
+QRcode::png("Optometer Optika", $qrcode, "H");      // https://phpqrcode.sourceforge.net
 
 echo "<img src='" . $qrcode . "'>";
+
+echo "<pre>";
 echo $qrcode;
+echo "</pre>";
 
 
 
 
 
 
-// https://phpqrcode.sourceforge.net/index.php
+
+
+
+
+// if not working (..Call to undefined function imagecreate() in..) 
+//  - open c:\xampp\php\php.ini
+//  - find ;extension=gd               and remove ;(semicolon)
+//  - find ;gd.jpeg_ignore_warning = 1 and remove ;(semicolon)
+//  - stop and start Apache
