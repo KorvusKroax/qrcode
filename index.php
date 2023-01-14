37ce546d00b2f3549ceba3ee27f1dc9907a336e4
Korvus <korvuskroax@gmail.com>
2023-01-14 22:45:52 +0100

    wip

--- index.php
+++ index.php
@@ -1,3 +1,37 @@
+<?php
+    include_once("phpqrcode/qrlib.php");
+
+    $content = "";
+
+    if (isset($_POST["submit"])) 
+    {
+        $content = $_POST["content"];
+
+        $path = "img/";
+        $filename = time() . ".png";
+        $qrcode_image = $path . $filename;
+
+        $qrcode_content = "https://optometeroptika.hu/qrcode/" . $content;
+
+        QRcode::png($qrcode_content, $qrcode_image, "H", 10);      // https://phpqrcode.sourceforge.net
+
+        echo "<img src='" . $qrcode_image . "'>";
+
+        echo "<pre>";
+        echo "content: " . $qrcode_content . "\n\n";
+        echo "</pre>";
+        echo "<hr>";
+    }
+
+    // if not working (..Call to undefined function imagecreate() in..)
+    // - open c:\xampp\php\php.ini
+    // - find ;extension=gd and remove ;(semicolon)
+    // - find ;gd.jpeg_ignore_warning = 1 and remove ;(semicolon)
+    // - stop and start Apache
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,12 +44,20 @@
     <title>QR code</title>
 </head>
 
+<style>
+    body {
+        text-align: center;
+    }
+</style>
+
 <body>
-        <h1>QR code</h1>
-        <form action="qrcode.php" method="post">
-            <input type="text" name="content" required>
-            <input type="submit" name="submit" value="submit">
-        </form>
+    <h1>QR code generator</h1>
+    <br>
+    <form method="post">
+        <span>QR code content:</span>
+        <pre>https://optometeroptika.hu/qrcode/<input type="text" name="content" value="<?= $content ?>" required></pre>
+        <input type="submit" name="submit" value="submit">
+    </form>
 </body>
 
 </html>
